cd80e1cea6720eb3b9b81fdd57f82a540225f50f
shack4 <shackman96@yahoo.com>
2015-09-01 13:14:34 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by abhishekde on 9/1/15.
+ */
+
+// INSTALL node.js
+    // Set IDE to node.js install location .... /usr/local/bin
+// *In Terminal* npm install --save express@4.10.2
+// *In Terminal* npm install --save socket.io
+
+// *Run Server(make sure "cd" is set to project folder)* node index.js
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require("socket.io")( http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+//test code
+/*io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});*/
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit("chat message", msg);
+        //console.log('message: ' + msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+
+
